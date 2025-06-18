89d398bd391d6086fe061ff610bc6bd76d2ebb60
Pauline Moraa <pmoraa431@gmail.com>
2025-06-18 13:44:13 +0300

    created resource folder with files

--- app.py
+++ app.py
@@ -9,17 +9,19 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Basic configuration
+#configurations
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI", "sqlite:///linksaver.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# Initialize database and migrations
+#database and migrations
 db.init_app(app)
 migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
     return {"message": "Welcome to the simplified LinkSaver App"}
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
