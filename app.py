12dfcffdc8ee3f75efb973333e5d73625262545d
Pauline Moraa <pmoraa431@gmail.com>
2025-06-24 18:03:06 +0300

    added run server

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
 import os
-from flask import Flask
 from datetime import timedelta
+
+from flask import Flask
 from flask_restful import Api, Resource
+from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
-from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
+
+# Import models and database
 from models import db
-#import resources
+
+# from resources.users import SignUpResource, LoginResource
+# from resources.links import LinkListResource, LinkResource
+# from resources.categories import CategoryListResource
 
 
 load_dotenv()
 
 app = Flask(__name__)
-
 CORS(app)
-
 api = Api(app)
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///linksaver.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET", "super-secret-key")
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
 app.config["BUNDLE_ERRORS"] = True
+
 db.init_app(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
@@ -34,7 +40,7 @@ class Index(Resource):
         return {"message": "Welcome to the LinkSaver API"}
 
 
-api.add_resource(Index, "/")
+# api.add_resource(Index, "/")
 # api.add_resource(SignUpResource, "/signup")
 # api.add_resource(LoginResource, "/login")
 # api.add_resource(LinkListResource, "/links")
