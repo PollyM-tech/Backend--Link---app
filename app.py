c7359962f5096647c3f8bca52b58de8fe599148d
Daud Abdiwahab <daudwahab1320@gmail.com>
2025-06-24 17:17:33 +0300

    added imports,setup cors&flask_restful

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from models import db
-from dotenv import load_dotenv
 import os
+from flask import Flask
+from datetime import timedelta
+from flask_restful import Api, Resource
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+
+
+from models import db
+#import resources
+
 
 load_dotenv()
 
 app = Flask(__name__)
+
+CORS(app)
+
+api = Api(app)
 
 #configurations
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI", "sqlite:///linksaver.db")
