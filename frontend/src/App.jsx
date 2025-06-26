2c97bdfbfc5e56cd9295590c08de369fb024a335
Tabitha.W.N <twangechi742@gmail.com>
2025-06-26 12:20:24 +0300

    Updated  router App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
