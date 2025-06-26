5a7c2d7ecadcbc908b90ac5b57ae229b49b61a29
Tabitha.W.N <twangechi742@gmail.com>
2025-06-26 12:17:53 +0300

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,29 @@ import { Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
+    <Router>
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/main" element={<MainPage />} />
     </Routes>
+    <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark" 
+      />
+    </Router>
   );
 };
 
